fce780272a7c785c99dd5e64c8af8bbea461d42c
Laura Trager <laura.trager@gmail.com>
2017-10-30 17:03:01 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,15 +1,15 @@
-
+collection = ["1", "2", "3"]
 
 def my_collect(collection)
+  new_collection = []
   index = 0
 
   while index < collection.length
     yield(collection[index])
-    index += 1
+    new_collection << index += 1
   end
-  collection
 end
 
-my_collect(["1", "2", "3"]) do |element|
+my_collect(collection) do |element|
   puts "Hello there. You are number #{element}!"
 end
